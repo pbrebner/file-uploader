d3275dc66b39f6c9fee5d0f90e549ba9799a8821
Patrick Brebner <patrick.brebner@gmail.com>
2025-01-20 16:17:29 -0800

    Added prisma session store

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+const prisma = require("./prisma/initiate");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 
 const app = express();
 
@@ -14,13 +16,27 @@ app.set("view engine", "ejs");
 
 app.use(
     session({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+        },
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
+        store: new PrismaSessionStore(prisma, {
+            checkPeriod: 2 * 60 * 1000, //ms
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }),
     })
 );
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+
+// Set up to be able to access currentUser from local variables
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.use("/", indexRouter);
 
