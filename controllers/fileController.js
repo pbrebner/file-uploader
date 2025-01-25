1e831efd1020e3fe225a8fc9c3d4a6a5ee346568
Patrick Brebner <patrick.brebner@gmail.com>
2025-01-25 15:37:13 -0800

    Add multer to upload and delete files

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -2,6 +2,19 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 const prisma = require("../prisma/initiate");
+const fs = require("fs");
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e3);
+        cb(null, file.fieldname + "-" + uniqueSuffix);
+    },
+});
+const upload = multer({ storage: storage });
 
 // Gets all files of folder
 exports.getFiles = asyncHandler(async (req, res, next) => {
@@ -28,13 +41,28 @@ exports.getFiles = asyncHandler(async (req, res, next) => {
 });
 
 // Creates a new file in folder
-// TODO: ADD SIZE AND PATH
 exports.createFile = [
+    upload.single("fileUpload"),
     body("fileName")
         .trim()
-        .isLength({ min: 1, max: 30 })
-        .withMessage("File Name must be between 1 and 30 characters long.")
+        .isLength({ max: 30 })
+        .withMessage("File Name can't be more than 30 characters long.")
+        .optional()
         .escape(),
+    body("fileUpload")
+        .trim()
+        .custom((value, { req }) => {
+            const file = req.file;
+            const allowedSize = 500000; // 5MB file size limit
+
+            if (!file) {
+                throw new Error("Please add a file to upload.");
+            } else if (file.size > allowedSize) {
+                throw new Error("File size is too large. 5MB maximum.");
+            } else {
+                return true;
+            }
+        }),
     asyncHandler(async (req, res, next) => {
         if (req.user) {
             const errors = validationResult(req);
@@ -45,6 +73,7 @@ exports.createFile = [
                         id: Number(req.params.folderId),
                     },
                 });
+
                 const files = await prisma.file.findMany({
                     where: {
                         folderId: Number(req.params.folderId),
@@ -60,11 +89,14 @@ exports.createFile = [
             } else {
                 await prisma.file.create({
                     data: {
-                        fileName: req.body.fileName,
+                        fileName: req.body.fileName || req.file.filename,
+                        size: req.file.size,
+                        path: req.file.path,
                         folderId: Number(req.params.folderId),
                         userId: req.user.id,
                     },
                 });
+                res.redirect(`/folders/${req.params.folderId}/files`);
             }
         } else {
             res.redirect("/");
@@ -79,14 +111,10 @@ exports.getFile = asyncHandler(async (req, res, next) => {
             where: {
                 id: Number(req.params.fileId),
             },
-            include: {
-                folder: true,
-            },
         });
 
         res.render("file", {
             title: file.fileName,
-            folder: file.folder,
             file: file,
         });
     } else {
@@ -116,6 +144,14 @@ exports.deleteFile = asyncHandler(async (req, res, next) => {
                     id: Number(req.params.fileId),
                 },
             });
+
+            // Delete file from file system
+            fs.unlink(file.path, (err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+
             res.redirect(`/folders/${req.params.folderId}/files`);
         }
     } else {
